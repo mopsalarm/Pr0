8c0e3bbcdcf747ec0d14efb3c99c91fd6b65c684
Mopsalarm <mopsalarm@example.com>
2015-02-28 00:23:21 +0100

    Verbesserungen für VideoPlayback, auch bei älteren Android-Versionen

--- app/src/main/java/com/pr0gramm/app/PostFragment.java
+++ app/src/main/java/com/pr0gramm/app/PostFragment.java
@@ -1,12 +1,16 @@
 package com.pr0gramm.app;
 
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.net.rtp.AudioStream;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +29,7 @@ import com.pr0gramm.app.feed.FeedService;
 import com.pr0gramm.app.feed.Vote;
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
@@ -34,6 +39,7 @@ import javax.inject.Inject;
 import pl.droidsonroids.gif.GifDrawable;
 import roboguice.fragment.RoboFragment;
 import roboguice.inject.InjectView;
+import rx.Observable;
 import rx.schedulers.Schedulers;
 import rx.util.async.Async;
 
@@ -120,81 +126,94 @@ public class PostFragment extends RoboFragment {
                     activity.onScrollHideToolbarListener.onScrolled(top - oldTop));
         }
 
-        // load the image
-        String image = "http://img.pr0gramm.com/" + item.getItem().getImage();
-        if (image.endsWith(".webm")) {
+        // get the url of the posts content (image or video)
+        String url = "http://img.pr0gramm.com/" + item.getItem().getImage();
+        if (url.toLowerCase().endsWith(".webm")) {
             // hide the image view
             viewImage.setVisibility(View.GONE);
             viewVideo.setVisibility(View.VISIBLE);
 
-            // hide video controls
-            MediaController ctrl = new MediaController(getActivity());
-            ctrl.setVisibility(View.GONE);
-            viewVideo.setMediaController(ctrl);
-
-            viewVideo.setVideoURI(Uri.parse(image));
-            viewVideo.start();
-            viewVideo.setOnCompletionListener(mp -> {
-                // start from the beginning
-                viewVideo.seekTo(0);
-                viewVideo.start();
-            });
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                // full size for videos.
-                viewVideo.setOnInfoListener(new MediaPlayer.OnInfoListener() {
-                    int lastWidth = 0;
-
-                    @Override
-                    public boolean onInfo(MediaPlayer mp, int what, int extra) {
-                        int width = mp.getVideoWidth();
-                        int height = mp.getVideoHeight();
-
-                        // check if size is okay
-                        if (width <= 0 || width == lastWidth)
-                            return true;
-
-                        // remember!
-                        lastWidth = width;
-
-                        ViewParent parent = viewVideo.getParent();
-                        if (parent instanceof View) {
-                            int parentWidth = ((View) parent).getWidth();
-                            float aspect = width / (float) height;
-                            viewVideo.getLayoutParams().height = (int) (parentWidth / aspect);
-                            viewVideo.getParent().requestLayout();
-                        }
-
-                        return true;
-                    }
-                });
-            }
-        } else if (image.endsWith(".gif")) {
-            // hide the video player
-            viewVideo.stopPlayback();
-            viewVideo.setVisibility(View.GONE);
-            viewImage.setVisibility(View.VISIBLE);
-
-            bindFragment(this, Async.fromCallable(() -> {
-                byte[] bytes = Resources.toByteArray(new URL(image));
-                return new GifDrawable(bytes);
-            }, Schedulers.io())).subscribe(viewImage::setImageDrawable);
+            displayTypeVideo(url);
 
         } else {
-            // hide the video player
-            viewVideo.stopPlayback();
+            // hide the video view
             viewVideo.setVisibility(View.GONE);
             viewImage.setVisibility(View.VISIBLE);
 
-            picasso.load(image)
-                    .resize(1024, 1024)
-                    .centerInside()
-                    .onlyScaleDown()
-                    .into(viewImage);
+            if (url.toLowerCase().endsWith(".gif")) {
+                displayTypeGif(url);
+            } else {
+                displayTypeImage(url);
+            }
         }
 
+        // load post info (comments and tags)
         long id = item.getItem().getId();
         bindFragment(this, feedService.loadPostDetails(id)).subscribe(this::onPostReceived);
+    }
+
+    /**
+     * Displays the given image url. This will scale down the image to
+     * fit in the ui.
+     *
+     * @param image The image to load and display.
+     */
+    private void displayTypeImage(String image) {
+        picasso.load(image)
+                .resize(1024, 1024)
+                .centerInside()
+                .onlyScaleDown()
+                .into(viewImage);
+    }
+
+    /**
+     * Loads and displays a gif file.
+     *
+     * @param image The gif file to load.
+     */
+    private void displayTypeGif(String image) {
+        Observable<GifDrawable> loader = Async.fromCallable(() -> {
+            // load and decode imgage in background thread
+            byte[] bytes = Resources.toByteArray(new URL(image));
+            return new GifDrawable(bytes);
+        }, Schedulers.io());
+
+        // and set gif on ui thread as drawable
+        bindFragment(this, loader).subscribe(viewImage::setImageDrawable);
+    }
+
+    private void displayTypeVideo(String image) {
+        // hide video controls
+        MediaController ctrl = new MediaController(getActivity());
+        ctrl.setVisibility(View.GONE);
+        viewVideo.setMediaController(ctrl);
+
+        // set video on view
+        viewVideo.setVideoURI(Uri.parse(image));
+        viewVideo.start();
+
+        // start on play
+        viewVideo.setOnClickListener(v -> {
+            if (!viewVideo.isPlaying())
+                viewVideo.start();
+        });
+
+        viewVideo.setOnPreparedListener(mp -> {
+            mp.setLooping(true);
+
+            int width = mp.getVideoWidth();
+            int height = mp.getVideoHeight();
+
+            ViewParent parent = viewVideo.getParent();
+            if (parent instanceof View) {
+                int parentWidth = ((View) parent).getWidth();
+                float aspect = width / (float) height;
+
+                ViewGroup.LayoutParams params = viewVideo.getLayoutParams();
+                params.height = (int) (parentWidth / aspect);
+                viewVideo.setLayoutParams(params);
+            }
+        });
     }
 
     private void onPostReceived(Post post) {
