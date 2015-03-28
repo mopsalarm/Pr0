e35b151f3bcf6c3bee7472b8ad32a8f4cdcec338
Mopsalarm <mopsalarm@example.com>
2015-03-28 18:29:54 +0100

    Fixes

--- app/src/main/assets/sugar_upgrades/5.sql
+++ app/src/main/assets/sugar_upgrades/5.sql
@@ -1,7 +1,1 @@
-create table if not exists bookmark (
-    ID integer primary key autoincrement,
-    TITLE text not null,
-    FILTER_FEED_TYPE text not null,
-    FILTER_TAGS text,
-    FILTER_USERNAME text
-);
+create table if not exists bookmark (ID integer primary key autoincrement, TITLE, FILTER_FEED_TYPE, FILTER_TAGS, FILTER_USERNAME);
