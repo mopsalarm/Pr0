3c40fe8b8862b21d8410407e864de2ff97163cfb
Mopsalarm <mopsalarm@example.com>
2015-04-09 18:56:54 +0200

    Grundlagen für "Benachrichtigung bei neuen Nachrichten" gelegt

--- app/src/main/java/com/pr0gramm/app/SyncIntentService.java
+++ app/src/main/java/com/pr0gramm/app/SyncIntentService.java
@@ -1,10 +1,17 @@
 package com.pr0gramm.app;
 
+import android.app.Notification;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Stopwatch;
 import com.google.inject.Inject;
+import com.pr0gramm.app.api.pr0gramm.response.Sync;
 import com.pr0gramm.app.services.UserService;
 
 import roboguice.service.RoboIntentService;
@@ -13,6 +20,7 @@ import roboguice.service.RoboIntentService;
  */
 public class SyncIntentService extends RoboIntentService {
     private static final String TAG = "SyncIntentService";
+    private static final int ID_NEW_MESSAGE = 5001;
 
     @Inject
     private UserService userService;
@@ -30,7 +38,9 @@ public class SyncIntentService extends RoboIntentService {
         Stopwatch watch = Stopwatch.createStarted();
         try {
             Log.i("SyncIntentService", "performing sync");
-            userService.sync();
+            Optional<Sync> sync = userService.sync();
+            if(sync.isPresent() && sync.get().getInboxCount() > 0)
+                showInboxNotification(sync.get());
 
             Log.i("SyncIntentService", "updating info");
             userService.info();
@@ -44,5 +54,24 @@ public class SyncIntentService extends RoboIntentService {
         } finally {
             SyncBroadcastReceiver.completeWakefulIntent(intent);
         }
+    }
+
+    private void showInboxNotification(Sync sync) {
+        int iconId = R.drawable.ic_notify_unread;
+
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), iconId);
+
+        int color = getResources().getColor(R.color.primary);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("New message")
+                .setContentText("Something happened on pr0gramm")
+                .setSmallIcon(iconId)
+                .setLargeIcon(icon)
+                .setAutoCancel(true)
+                .setCategory(NotificationCompat.CATEGORY_EMAIL)
+                .setLights(color, 500, 500)
+                .build();
+
+        NotificationManagerCompat.from(this).notify(ID_NEW_MESSAGE, notification);
     }
 }
