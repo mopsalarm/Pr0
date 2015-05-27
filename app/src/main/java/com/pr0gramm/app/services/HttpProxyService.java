c01e21697d0543bd80780405121de014132ff688
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-05-27 21:41:08 +0200

    Updated NanoHTTPD to a new version

--- app/src/main/java/com/pr0gramm/app/services/HttpProxyService.java
+++ app/src/main/java/com/pr0gramm/app/services/HttpProxyService.java
@@ -1,8 +1,10 @@
 package com.pr0gramm.app.services;
 
 import android.net.Uri;
+import android.support.annotation.NonNull;
 
 import com.google.common.base.Charsets;
+import com.google.common.base.Optional;
 import com.google.common.collect.Iterables;
 import com.google.common.hash.Hashing;
 import com.google.common.io.BaseEncoding;
@@ -20,13 +22,15 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
+import fi.iki.elonen.NanoHTTPD;
+
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static java.lang.System.currentTimeMillis;
 
 /**
  */
 @Singleton
-public class HttpProxyService extends NanoHttpServer implements ProxyService {
+public class HttpProxyService extends NanoHTTPD implements ProxyService {
     private static final Logger logger = LoggerFactory.getLogger(HttpProxyService.class);
 
     private final String nonce;
@@ -73,15 +77,15 @@ public class HttpProxyService extends NanoHttpServer implements ProxyService {
         Uri uri = Uri.parse(session.getUri());
         if (!nonce.equals(Iterables.getFirst(uri.getPathSegments(), null))) {
             logger.info("Got request with invalid nonce: {}", uri);
-            return new Response(Response.Status.FORBIDDEN, "text/plain", "");
+            return newFixedLengthResponse(Response.Status.FORBIDDEN, "text/plain", "");
         }
 
-        String url = null;
+        String decodedUrl = null;
         try {
             String encodedUrl = uri.getLastPathSegment();
 
             logger.info("Decode {} as utf8 string now", encodedUrl);
-            url = new String(
+            String url = decodedUrl = new String(
                     BaseEncoding.base64Url().decode(encodedUrl),
                     Charsets.UTF_8).trim();
 
@@ -91,52 +95,95 @@ public class HttpProxyService extends NanoHttpServer implements ProxyService {
             com.squareup.okhttp.Response response = okHttpClient.newCall(request).execute();
 
             Response.IStatus status = translateStatus(response.code(), response.message());
-            String contentType = response.header("Content-Type", "application/octet-stream");
+            String contentType = response.header("Content-Type", guessContentType(url));
 
             InputStream stream;
-            final Integer length = Ints.tryParse(response.header("Content-Length", ""));
-            if (length != null && length > 0) {
-                stream = new FilterInputStream(response.body().byteStream()) {
-//                    int read = 0;
-//
-//                    @Override
-//                    public int read(byte[] buffer, int byteOffset, int byteCount) throws IOException {
-//                        int result = super.read(buffer, byteOffset, byteCount);
-//                        if(read / 100_000 != (read + result) / 100_000) {
-//                            logger.info(format("Approx %1d%% loaded", 100 * read / length));
-//                        }
-//
-//                        read += result;
-//                        return result;
-//                    }
+            final Optional<Integer> length = parseContentLength(response);
+            if (length.isPresent()) {
+                stream = ByteStreams.limit(new FilterInputStream(response.body().byteStream()) {
+                    int read = 0;
+                    int _length = length.get();
+
+                    @Override
+                    public int read(@NonNull byte[] buffer, int byteOffset, int byteCount) throws IOException {
+                        int result = super.read(buffer, byteOffset, byteCount);
+                        if (read / 500_000 != (read + result) / 500_000) {
+                            int percent = 100 * read / _length;
+                            int loaded = (read + result) / 1024;
+                            logger.info("Approx {}% loaded ({}, {}kb)", percent, url, loaded);
+                        }
+
+                        read += result;
+                        if (read == _length) {
+                            logger.info("Finished sending file {}", url);
+                        }
+
+                        return result;
+                    }
 
                     @Override
                     public int available() throws IOException {
                         // fake a large value here, so ByteStreams.limit can fix it :/
                         return Integer.MAX_VALUE;
                     }
-                };
-
-                stream = ByteStreams.limit(stream, length.longValue());
+                }, length.get().longValue());
             } else {
                 stream = response.body().byteStream();
             }
 
-            Response result = new Response(status, contentType, stream);
+            Response result = newFixedLengthResponse(status, contentType, stream, length.or(-1));
             result.addHeader("Accept-Range", "bytes");
-            result.addHeader("Connection", "close");
-            result.setChunkedTransfer(length == null);
 
             // forward content range header
             String contentRange = response.header("Content-Range");
             if (contentRange != null)
                 result.addHeader("Content-Range", contentRange);
 
+            if(length.isPresent()) {
+                result.addHeader("Content-Length", String.valueOf(length.get()));
+            }
+
+            logger.info("Start sending {} ({} kb)", url, length.or(-1) / 1024);
             return result;
 
         } catch (Exception e) {
-            logger.error("Could not proxy for url " + url, e);
-            return new Response(Response.Status.INTERNAL_ERROR, "text/plain", e.toString());
+            logger.error("Could not proxy for url " + decodedUrl, e);
+            return newFixedLengthResponse(Response.Status.INTERNAL_ERROR, "text/plain", e.toString());
+        }
+    }
+
+    /**
+     * Parses the content length. Will return null, if the length is not parsable
+     * or not a positive number.
+     *
+     * @return The content length.
+     */
+    private Optional<Integer> parseContentLength(com.squareup.okhttp.Response response) {
+        Integer parsed = Ints.tryParse(response.header("Content-Length", ""));
+        if (parsed != null && parsed <= 0) {
+            parsed = null;
+        }
+
+        return Optional.fromNullable(parsed);
+    }
+
+    /**
+     * Guess a content type from the URL.
+     *
+     * @param url The url to guess the content stream for.
+     */
+    private static String guessContentType(String url) {
+        url = url.toLowerCase();
+        if (url.endsWith(".webm")) {
+            return "video/webm";
+        } else if (url.matches(".*\\.jpe?g")) {
+            return "image/jpeg";
+        } else if (url.endsWith(".png")) {
+            return "image/png";
+        } else if (url.endsWith(".gif")) {
+            return "image/gif";
+        } else {
+            return "application/octet-stream";
         }
     }
 
