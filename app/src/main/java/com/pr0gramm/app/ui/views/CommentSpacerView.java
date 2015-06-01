63d45638dc589e01a2f6c3efe88887ef592a584b
schr3j <schr3j@users.noreply.github.com>
2015-06-01 20:00:58 +0200

    added CommentSpacerView renamed ic_launcher to ic_app because of some stupid library to fix build

---
+++ app/src/main/java/com/pr0gramm/app/ui/views/CommentSpacerView.java
@@ -0,0 +1,71 @@
+package com.pr0gramm.app.ui.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.pr0gramm.app.R;
+
+/**
+ * Created by jakob on 01.06.15.
+ */
+public class CommentSpacerView extends View {
+
+    private float lineMargin;
+    private float lineWidth;
+    private int lineColor;
+    private int depth;
+
+    private Paint linePaint;
+
+    public CommentSpacerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.CommentSpacerView, 0, 0);
+
+        try {
+            lineMargin = a.getDimension(R.styleable.CommentSpacerView_lineMargin, 10);
+            lineWidth = a.getDimension(R.styleable.CommentSpacerView_lineWidth, 2);
+            lineColor = a.getColor(R.styleable.CommentSpacerView_lineColor, 1);
+            depth = a.getInt(R.styleable.CommentSpacerView_depth, 1);
+        } finally {
+            a.recycle();
+        }
+
+        initDraw();
+    }
+
+    public void setDepth(int depth) {
+        this.depth = depth;
+        invalidate();
+        requestLayout();
+    }
+
+    public void initDraw() {
+        linePaint = new Paint();
+        linePaint.setColor(lineColor);
+        linePaint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        float x = 0;
+        for(int i = 1; i < depth; i++) {
+            x = lineMargin + (i - 1) * (lineMargin + lineWidth);
+            canvas.drawLine(x,0,x,getHeight(), linePaint);
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int height = MeasureSpec.getSize(heightMeasureSpec);
+
+        float width = (depth - 1) * (lineMargin + lineWidth);
+
+        setMeasuredDimension((int) width, height);
+    }
+}
