f8fce26cd6007ba423bce1548775810ab4159b5b
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-06-19 18:31:14 +0200

    Moved the drawing code for the software decoder into a drawable and decoupled the drawing speed from the decoding speed for a smoother result.

--- app/src/main/java/com/pr0gramm/app/ui/views/viewer/MpegMediaView.java
+++ app/src/main/java/com/pr0gramm/app/ui/views/viewer/MpegMediaView.java
@@ -3,11 +3,19 @@ package com.pr0gramm.app.ui.views.viewer;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.SystemClock;
 import android.widget.ImageView;
 
+import com.google.common.base.Optional;
 import com.google.common.util.concurrent.Uninterruptibles;
 import com.google.inject.Inject;
+import com.pr0gramm.app.Pr0grammApplication;
 import com.pr0gramm.app.R;
 import com.pr0gramm.app.mpeg.InputStreamCache;
 import com.pr0gramm.app.mpeg.PictureBuffer;
@@ -21,6 +29,9 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.ref.WeakReference;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -30,19 +41,20 @@ import rx.Subscription;
 import rx.schedulers.Schedulers;
 import rx.util.async.Async;
 
+import static com.pr0gramm.app.AndroidUtility.checkNotMainThread;
 import static com.pr0gramm.app.ui.dialogs.ErrorDialogFragment.defaultOnError;
 
 /**
  */
 @SuppressLint("ViewConstructor")
-public class MpegMediaView extends MediaView {
+public class MpegMediaView extends MediaView implements OnSizeCallback {
     @InjectView(R.id.image)
     private ImageView imageView;
 
     @Inject
     private Downloader downloader;
 
-    private Mpeg mpeg;
+    private VideoPlayer videoPlayer;
     private Subscription loading;
 
     public MpegMediaView(Context context, Binder binder, String url, Runnable onViewListener) {
@@ -50,37 +62,41 @@ public class MpegMediaView extends MediaView {
     }
 
     private void asyncLoadVideo() {
-        if (loading != null || mpeg != null) {
+        if (loading != null || videoPlayer != null) {
             return;
         }
 
-        Observable<Mpeg> loader = binder.bind(Async.fromCallable(() -> {
+        Observable<VideoPlayer> loader = binder.bind(Async.fromCallable(() -> {
             Downloader.Response response = downloader.load(Uri.parse(url), 0);
-            return new Mpeg(imageView, response.getInputStream(), this::onSizeChanged);
+            return new VideoPlayer(response.getInputStream(), this);
         }, Schedulers.io()));
 
         loading = loader.finallyDo(() -> loading = null).subscribe(mpeg -> {
             hideBusyIndicator();
 
-            this.mpeg = mpeg;
+            this.videoPlayer = mpeg;
+            imageView.setImageDrawable(this.videoPlayer.getVideoDrawable());
+
             if (isPlaying()) {
-                this.mpeg.play();
+                this.videoPlayer.play();
                 onMediaShown();
             }
 
         }, defaultOnError());
     }
 
-    private void onSizeChanged(Integer width, Integer height) {
+
+    @Override
+    public void onSizeChanged(int width, int height) {
         float viewAspect = (float) width / height;
-        setViewAspect(viewAspect);
+        post(() -> setViewAspect(viewAspect));
     }
 
     private void loadAndPlay() {
-        if (mpeg == null && loading == null) {
+        if (videoPlayer == null && loading == null) {
             asyncLoadVideo();
-        } else if (mpeg != null) {
-            mpeg.play();
+        } else if (videoPlayer != null) {
+            videoPlayer.play();
             onMediaShown();
         }
     }
@@ -91,9 +107,11 @@ public class MpegMediaView extends MediaView {
             loading = null;
         }
 
-        if (mpeg != null) {
-            mpeg.stop();
-            mpeg = null;
+        if (videoPlayer != null) {
+            videoPlayer.stop();
+
+            Pr0grammApplication.getRefWatcher().watch(videoPlayer);
+            videoPlayer = null;
         }
     }
 
@@ -108,8 +126,8 @@ public class MpegMediaView extends MediaView {
     @Override
     public void onPause() {
         super.onPause();
-        if (mpeg != null && isPlaying())
-            mpeg.pause();
+        if (videoPlayer != null && isPlaying())
+            videoPlayer.pause();
     }
 
     @Override
@@ -134,22 +152,117 @@ public class MpegMediaView extends MediaView {
     }
 
 
-    private static class Mpeg implements VideoConsumer {
-        private static final Logger logger = LoggerFactory.getLogger(Mpeg.class);
+    private static class MpegVideoDrawable extends Drawable implements Runnable {
+        private final Object lock = new Object();
+
+        private long delay = 1000 / 30;
+
+        private volatile boolean running;
+        private volatile Bitmap current;
+        private final BlockingQueue<Bitmap> next = new SynchronousQueue<>();
+        private final BlockingQueue<Bitmap> previous = new ArrayBlockingQueue<>(4);
+
+        public void start() {
+            running = true;
+            scheduleSelf(this, SystemClock.uptimeMillis() + delay);
+        }
+
+        public void stop() {
+            running = false;
+            unscheduleSelf(this);
+        }
+
+        public void push(Bitmap frame) throws InterruptedException {
+            checkNotMainThread();
+            next.put(frame);
+        }
+
+        public Optional<Bitmap> pull() {
+            checkNotMainThread();
+
+            try {
+                return Optional.fromNullable(previous.poll(100, TimeUnit.MILLISECONDS));
+            } catch (InterruptedException e) {
+                return Optional.absent();
+            }
+        }
+
+        public void setPictureDelay(long delay) {
+            this.delay = delay;
+        }
+
+        @Override
+        public void run() {
+            moveToNextFrame();
+
+            if (current != null) {
+                invalidateSelf();
+            }
+
+            if (running) {
+                start();
+            }
+        }
+
+        private void moveToNextFrame() {
+            Bitmap frame = this.next.poll();
+            if (frame != null) {
+                if (current != null) {
+                    previous.add(current);
+                }
+
+                synchronized (lock) {
+                    current = frame;
+                }
+            }
+        }
+
+        @Override
+        public void draw(Canvas canvas) {
+            synchronized (lock) {
+                if (current != null) {
+                    Rect bounds = getBounds();
+                    canvas.drawBitmap(current, null, bounds, null);
+                }
+            }
+        }
+
+        @Override
+        public void setAlpha(int alpha) {
+            // do nothing
+        }
+
+        @Override
+        public void setColorFilter(ColorFilter cf) {
+            // do nothing
+        }
+
+        @Override
+        public int getOpacity() {
+            return PixelFormat.OPAQUE;
+        }
+    }
+
+    private static class VideoPlayer implements VideoConsumer {
+        private static final Logger logger = LoggerFactory.getLogger(VideoPlayer.class);
 
         private final AtomicBoolean running = new AtomicBoolean();
         private final AtomicBoolean paused = new AtomicBoolean();
-        private final WeakReference<ImageView> imageView;
         private final WeakReference<OnSizeCallback> sizeCallback;
 
         private final InputStreamCache cache;
-        private volatile Bitmap visible, backbuffer;
-        private PictureBuffer buffer;
+        private final MpegVideoDrawable drawable = new MpegVideoDrawable();
 
-        public Mpeg(ImageView imageView, InputStream inputStream, OnSizeCallback sizeCallback) {
-            this.imageView = new WeakReference<>(imageView);
+        private PictureBuffer buffer;
+        private int bitmaps;
+
+        public VideoPlayer(InputStream inputStream, OnSizeCallback sizeCallback) {
             this.sizeCallback = new WeakReference<>(sizeCallback);
             this.cache = new InputStreamCache(inputStream);
+        }
+
+        public Drawable getVideoDrawable() {
+            return drawable;
         }
 
         public void play() {
@@ -158,18 +271,25 @@ public class MpegMediaView extends MediaView {
                 Thread thread = new Thread(this::playLoop);
                 thread.setDaemon(true);
                 thread.setName("MpegPlayerThread:" + this);
+                thread.setPriority(3);
                 thread.start();
+
+                drawable.start();
             }
         }
 
         private void playLoop() {
-            while (running.get()) {
-                try {
-                    playOnce();
-                } catch (IOException error) {
-                    running.set(false);
-                    break;
+            try {
+                while (running.get()) {
+                    try {
+                        playOnce();
+                    } catch (IOException error) {
+                        running.set(false);
+                        break;
+                    }
                 }
+            } finally {
+                drawable.stop();
             }
         }
 
@@ -213,59 +333,50 @@ public class MpegMediaView extends MediaView {
 
         @Override
         public void pictureDecoded(PictureBuffer picture) {
-            if (!running.get()) {
-                logger.info("got picture, but stop was requested");
-                throw new VideoPlaybackStoppedException();
+            Bitmap bitmap;
+            if (bitmaps < 2) {
+                ensureStillRunning();
+                bitmap = Bitmap.createBitmap(
+                        picture.width, picture.height, Bitmap.Config.ARGB_8888);
+
+                bitmaps += 1;
+
+            } else {
+                do {
+                    ensureStillRunning();
+                    bitmap = drawable.pull().orNull();
+                } while (bitmap == null);
             }
 
-            // logger.info("picture decoded, ready to show", picture);
-            ImageView imageView = this.imageView.get();
-            if (imageView != null) {
-                if (backbuffer == null) {
-                    backbuffer = Bitmap.createBitmap(
-                            picture.width, picture.height, Bitmap.Config.ARGB_8888);
-                }
+            // post information about the newly received size info
+            OnSizeCallback callback = sizeCallback.get();
+            if (callback != null) {
+                callback.onSizeChanged(picture.width, picture.height);
+            }
 
-                backbuffer.setPixels(picture.pixels, 0, picture.codedWidth, 0, 0,
-                        picture.width, picture.height);
+            bitmap.setPixels(picture.pixels, 0, picture.codedWidth, 0, 0,
+                    picture.width, picture.height);
 
-                imageView.post(() -> {
-                    // logger.info("show picture in ui now");
-                    if (imageView.getWindowToken() != null) {
-                        imageView.setImageBitmap(backbuffer);
-
-                        // set the aspect
-                        OnSizeCallback sizeCallback = this.sizeCallback.get();
-                        sizeCallback.onSizeChanged(backbuffer.getWidth(), backbuffer.getHeight());
-
-                        // switch back & front buffer
-                        Bitmap previous = visible;
-                        visible = backbuffer;
-                        backbuffer = previous;
-                    } else {
-                        stop();
-                    }
-                });
+            try {
+                drawable.push(bitmap);
+            } catch (InterruptedException err) {
+                stop();
+                throw new VideoPlaybackStoppedException();
             }
         }
 
         @Override
         public PictureBuffer fetchBuffer(VideoDecoder decoder) throws InterruptedException {
-            // check again for end of video
-            if (!running.get())
-                throw new VideoPlaybackStoppedException();
+            ensureStillRunning();
 
-            do {
-                // wait a moment to play video in the correct speed.
-                int delay = (int) (1000 / decoder.getPictureRate());
-                Uninterruptibles.sleepUninterruptibly(delay, TimeUnit.MILLISECONDS);
+            // set the current delay
+            drawable.setPictureDelay((long) (1000 / decoder.getPictureRate()));
 
-                // check again
-                if (!running.get())
-                    throw new VideoPlaybackStoppedException();
-
-                // if we are on pause, we'll loop
-            } while (paused.get());
+            // do nothing while paused.
+            while (paused.get()) {
+                ensureStillRunning();
+                Uninterruptibles.sleepUninterruptibly(100, TimeUnit.MILLISECONDS);
+            }
 
             if (buffer == null) {
                 int width = decoder.getWidth();
@@ -280,11 +391,14 @@ public class MpegMediaView extends MediaView {
             return buffer;
         }
 
-        private static class VideoPlaybackStoppedException extends RuntimeException {
+        private void ensureStillRunning() {
+            if (!running.get()) {
+                drawable.stop();
+                throw new VideoPlaybackStoppedException();
+            }
         }
 
-        public interface OnSizeCallback {
-            void onSizeChanged(int width, int height);
+        private static class VideoPlaybackStoppedException extends RuntimeException {
         }
     }
 }
---
+++ app/src/main/java/com/pr0gramm/app/ui/views/viewer/OnSizeCallback.java
@@ -0,0 +1,8 @@
+package com.pr0gramm.app.ui.views.viewer;
+
+/**
+ * Callback to receive sizes.
+ */
+public interface OnSizeCallback {
+    void onSizeChanged(int width, int height);
+}
