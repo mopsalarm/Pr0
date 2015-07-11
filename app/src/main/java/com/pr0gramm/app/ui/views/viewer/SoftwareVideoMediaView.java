2bb54e352cee7682fdc58f1870a6ba9510c5a3f7
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-07-11 13:08:47 +0200

    Moved mpeg decoder to its own file.

--- app/src/main/java/com/pr0gramm/app/ui/views/viewer/SoftwareVideoMediaView.java
+++ app/src/main/java/com/pr0gramm/app/ui/views/viewer/SoftwareVideoMediaView.java
@@ -2,19 +2,14 @@ package com.pr0gramm.app.ui.views.viewer;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.widget.ImageView;
 
 import com.google.inject.Inject;
 import com.pr0gramm.app.Pr0grammApplication;
 import com.pr0gramm.app.R;
-import com.pr0gramm.app.mpeg.PictureBuffer;
-import com.pr0gramm.app.mpeg.VideoConsumer;
-import com.pr0gramm.app.mpeg.VideoDecoder;
+import com.pr0gramm.app.mpeg.MpegSoftwareMediaPlayer;
 import com.pr0gramm.app.vpx.WebmMediaPlayer;
 import com.squareup.picasso.Downloader;
-
-import java.io.InputStream;
 
 import roboguice.inject.InjectView;
 import rx.Observable;
@@ -144,72 +139,4 @@ public class SoftwareVideoMediaView extends MediaView {
         super.onDestroy();
     }
 
-    private static class MpegSoftwareMediaPlayer extends SoftwareMediaPlayer implements VideoConsumer {
-        private PictureBuffer buffer;
-
-        public MpegSoftwareMediaPlayer(InputStream inputStream) {
-            super(inputStream);
-        }
-
-        @Override
-        protected void playOnce(InputStream stream) throws Exception {
-            logger.info("creating video decoder instance");
-            VideoDecoder decoder = new VideoDecoder(this, stream);
-
-            logger.info("start decoding mpeg file now");
-            decoder.decodeSequence();
-
-            logger.info("decoding mepg stream finished");
-        }
-
-        @Override
-        public void sequenceStarted() {
-            logger.info("sequence started");
-        }
-
-        @Override
-        public void sequenceEnded() {
-            logger.info("sequence ended");
-        }
-
-        @Override
-        public void pictureDecoded(PictureBuffer picture) {
-            Bitmap bitmap = requestBitmap(picture.width, picture.height);
-            try {
-                // post information about the newly received size info
-                reportSize(picture.width, picture.height);
-
-                bitmap.setPixels(picture.pixels, 0, picture.codedWidth, 0, 0,
-                        picture.width, picture.height);
-            } catch (Exception error) {
-                returnBitmap(bitmap);
-                throw error;
-            }
-
-            publishBitmap(bitmap);
-        }
-
-        @Override
-        public PictureBuffer fetchBuffer(VideoDecoder decoder) throws InterruptedException {
-            ensureStillRunning();
-
-            // set the current delay
-            publishFrameDelay((long) (1000 / decoder.getPictureRate()));
-
-            // do nothing while paused.
-            blockWhilePaused();
-
-            if (buffer == null) {
-                int width = decoder.getWidth();
-                int height = decoder.getHeight();
-                int codedWidth = decoder.getCodedWidth();
-                int codedHeight = decoder.getCodedHeight();
-
-                // logger.info("requesting buffer at {}x{} ({}x{})", width, height, codedWidth, codedHeight);
-                buffer = new PictureBuffer(width, height, codedWidth, codedHeight);
-            }
-
-            return buffer;
-        }
-    }
 }
