d39afeef046cba18862d52c62cdcff73f0419298
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-08-16 15:12:42 +0200

    Add tracking for new settings.

--- app/src/main/java/com/pr0gramm/app/Track.java
+++ app/src/main/java/com/pr0gramm/app/Track.java
@@ -101,6 +101,8 @@ public final class Track {
                 .putCustomAttribute("repost hint", String.valueOf(settings.markRepostsInFeed()))
                 .putCustomAttribute("notifications", String.valueOf(settings.showNotifications()))
                 .putCustomAttribute("mark images", settings.seenIndicatorStyle().name())
+                .putCustomAttribute("image size", settings.maxImageSize())
+                .putCustomAttribute("video progress", String.valueOf(settings.showVideoProgress()))
                 .putCustomAttribute("https", String.valueOf(settings.useHttps()))));
     }
 
