8cfe611b96219f85bf5f06d0730873f1260799bb
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-10-13 19:10:03 +0200

    Shorten links like on website.

--- app/src/main/java/com/pr0gramm/app/util/AndroidUtility.java
+++ app/src/main/java/com/pr0gramm/app/util/AndroidUtility.java
@@ -66,8 +66,8 @@ public class AndroidUtility {
     private static final Logger logger = LoggerFactory.getLogger(AndroidUtility.class);
 
     private static final Pattern RE_USERNAME = Pattern.compile("@[A-Za-z0-9]+");
-    private static final Pattern RE_GENERIC_LINK = Pattern.compile("https?://pr0gramm\\.com(/(?:new|top|user)/[^ ]*[0-9])");
-    private static final Pattern RE_GENERIC_SHORT_LINK = Pattern.compile("/(new|top|user)/[^ ]*[0-9]");
+    private static final Pattern RE_GENERIC_LINK = Pattern.compile("https?://pr0gramm\\.com(/(?:new|top|user)/[^\\p{javaWhitespace}]*[0-9])");
+    private static final Pattern RE_GENERIC_SHORT_LINK = Pattern.compile("/(new|top|user)/[^\\p{javaWhitespace}]*[0-9]");
 
     private AndroidUtility() {
     }
