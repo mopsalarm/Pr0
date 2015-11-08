02317ae73af2259aafbd3c20125cf5e36b221c44
schr3j <schr3j@users.noreply.github.com>
2015-11-08 15:06:57 +0100

    new, responsive website

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var scss = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var jade = require('gulp-jade');
+var plumber = require('gulp-plumber');
+
+gulp.task('default', ['scss', 'jade']);
+
+gulp.task('watch', function() {
+  gulp.watch('scss/**/*.scss', ['scss']);
+  gulp.watch('jade/**/*.jade', ['jade']);
+});
+
+gulp.task('scss', function() {
+  gulp.src('scss/style.scss')
+    .pipe(plumber())
+    .pipe(scss().on('error', scss.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('jade', function() {
+  return gulp.src(['jade/index.jade'])
+    .pipe(plumber())
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest('.'));
+});
