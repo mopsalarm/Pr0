a835ba79b0ccf13626b0f78435fedde6348bfffd
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-11-09 22:14:45 +0100

    Updated list of most used tags.

--- app/src/main/java/com/pr0gramm/app/ui/TagInputView.java
+++ app/src/main/java/com/pr0gramm/app/ui/TagInputView.java
@@ -1,18 +1,13 @@
 package com.pr0gramm.app.ui;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.widget.ArrayAdapter;
 import android.widget.MultiAutoCompleteTextView;
 
 import com.google.common.base.Charsets;
-import com.google.common.base.Predicates;
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.ListMultimap;
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
-import com.pr0gramm.app.Pr0grammApplication;
-import com.pr0gramm.app.util.Lazy;
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.CharStreams;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.List;
 
@@ -32,44 +26,44 @@ public class TagInputView {
     private TagInputView() {
     }
 
-    private static final Type listOfStringsType = new TypeToken<List<String>>() {
-    }.getType();
-
     /**
-     * Parses the list of tags.
+     * Parses the list of tags. It is provided as one tag per line in utf8 encoding.
      */
     @SuppressLint("NewApi")
-    private static final Lazy<List<String>> TAGS = Lazy.of(() -> {
-        try (InputStream stream = Pr0grammApplication.GLOBAL_CONTEXT.getAssets().open("tags.json")) {
+    private static List<String> loadTagJson(Context context) {
+        try (InputStream stream = context.getAssets().open("tags.txt")) {
             try (Reader reader = new InputStreamReader(stream, Charsets.UTF_8)) {
-                List<String> tagList = new Gson().fromJson(reader, listOfStringsType);
+                ImmutableList.Builder<String> result = ImmutableList.builder();
+                for (String line : CharStreams.readLines(reader)) {
+                    line = line.trim();
+                    if (line.length() > 1) {
+                        result.add(line.trim());
+                    }
+                }
 
-                // group by lower-case version of each tag
-                ListMultimap<String, String> byLowerCase = FluentIterable
-                        .from(tagList)
-                        .index(String::toLowerCase);
-
-                // and remove duplicates by getting the first occurrence of each
-                // groups values
-                return FluentIterable.from(byLowerCase.asMap().entrySet())
-                        .transform(e -> Iterables.getFirst(e.getValue(), null))
-                        .filter(Predicates.notNull())
-                        .toList();
+                return result.build();
             }
-
         } catch (Exception error) {
             logger.error("Could not load list of tags", error);
             return Collections.emptyList();
         }
-    });
+    }
 
     public static void setup(MultiAutoCompleteTextView tagInput) {
+        if (TAGS == null) {
+            // load the tags from the compressed json file
+            TAGS = loadTagJson(tagInput.getContext());
+        }
+
         // get the auto-suggestion list.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(tagInput.getContext(),
                 android.R.layout.simple_dropdown_item_1line,
-                TAGS.get());
+                TAGS);
 
         tagInput.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
         tagInput.setAdapter(adapter);
     }
+
+    // holds the list of tags
+    private static List<String> TAGS;
 }
