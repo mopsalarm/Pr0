113a7be0cd9673c5af3adf92b7f8e693b6be0319
Mopsalarm <mopsalarm@users.noreply.github.com>
2015-12-31 18:58:51 +0100

    Move to 1:15 before reducing scale.

--- app/src/main/java/com/pr0gramm/app/ui/views/viewer/ImageMediaView.java
+++ app/src/main/java/com/pr0gramm/app/ui/views/viewer/ImageMediaView.java
@@ -3,7 +3,6 @@ package com.pr0gramm.app.ui.views.viewer;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
@@ -12,8 +11,8 @@ import com.pr0gramm.app.BuildConfig;
 import com.pr0gramm.app.R;
 import com.pr0gramm.app.Settings;
 import com.pr0gramm.app.services.SingleShotService;
-import com.pr0gramm.app.ui.DialogBuilder;
 import com.pr0gramm.app.ui.ImageDecoders;
+import com.pr0gramm.app.util.AndroidUtility;
 import com.squareup.picasso.Downloader;
 import com.squareup.picasso.Picasso;
 
@@ -29,6 +28,9 @@ import butterknife.Bind;
 @SuppressLint("ViewConstructor")
 public class ImageMediaView extends MediaView {
     private static final Logger logger = LoggerFactory.getLogger("ImageMediaView");
+
+    // we cap the image if it is more than 15 times as high as it is wide
+    private static final float CAP_IMAGE_RATIO = 1.f / 15.f;
 
     private final String tag = "ImageMediaView" + System.identityHashCode(this);
     private final boolean zoomView;
@@ -58,12 +60,12 @@ public class ImageMediaView extends MediaView {
 
         zoomView = findViewById(R.id.tabletlayout) != null;
 
-        if (isZoomView()) {
+        if (zoomView) {
             logger.info("Media view has a zoomview now");
         }
 
         imageView.setDebug(BuildConfig.DEBUG);
-        imageView.setZoomEnabled(isZoomView());
+        imageView.setZoomEnabled(zoomView);
 
         imageView.setBitmapDecoderFactory(() -> new ImageDecoders.PicassoDecoder(tag, picasso));
         imageView.setRegionDecoderFactory(() -> new ImageDecoders.PicassoRegionDecoder(downloader));
@@ -83,12 +85,12 @@ public class ImageMediaView extends MediaView {
             @Override
             public void onReady() {
                 float ratio = imageView.getSWidth() / (float) imageView.getSHeight();
-                float ratioCapped = Math.max(ratio, 1 / 10.f);
+                float ratioCapped = Math.max(ratio, CAP_IMAGE_RATIO);
 
                 setViewAspect(ratioCapped);
 
                 float maxScale = imageView.getWidth() / (float) imageView.getSWidth();
-                float minScale = isZoomView()
+                float minScale = zoomView
                         ? imageView.getHeight() / (float) imageView.getSHeight()
                         : maxScale * (ratio / ratioCapped);
 
@@ -106,13 +108,9 @@ public class ImageMediaView extends MediaView {
 
     @Override
     public void setViewAspect(float viewAspect) {
-        if (!isZoomView()) {
+        if (!zoomView) {
             super.setViewAspect(viewAspect);
         }
-    }
-
-    private boolean isZoomView() {
-        return zoomView;
     }
 
     @Override
@@ -127,79 +125,15 @@ public class ImageMediaView extends MediaView {
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
         picasso.cancelTag(tag);
-        ((ViewGroup) imageView.getParent()).removeView(imageView);
+        AndroidUtility.removeView(imageView);
+
+        super.onDestroy();
     }
 
-    /**
-     * We need to wrap the fragment into a weak reference so that the callback
-     * will not create a memory leak.
-     */
-//    private static class ImageCallback implements Callback {
-//        private final WeakReference<ImageMediaView> fragment;
-//
-//        public ImageCallback(ImageMediaView fragment) {
-//            this.fragment = new WeakReference<>(fragment);
-//        }
-//
-//        @Override
-//        public void onSuccess() {
-//            ImageMediaView player = fragment.get();
-//            if (player != null) {
-//                Drawable drawable = player.imageView.getDrawable();
-//                float imageAspect = (float) drawable.getIntrinsicWidth() / drawable.getIntrinsicHeight();
-//                player.setViewAspect(imageAspect);
-//
-//                if (player.isZoomView()) {
-//                    showZoomableImage(player, drawable);
-//                }
-//
-//                player.hideBusyIndicator();
-//                player.onMediaShown();
-//
-//                if (drawable.getIntrinsicHeight() / (float) drawable.getIntrinsicWidth() > 3) {
-//                    player.showMussteScrollenPopup();
-//                }
-//            }
-//        }
-//
-//        @Override
-//        public void onError() {
-//            ImageMediaView player = fragment.get();
-//            if (player != null) {
-//                player.hideBusyIndicator();
-//                player.showErrorIndicator();
-//            }
-//        }
-//    }
-
-//    private static void showZoomableImage(ImageMediaView player, Drawable drawable) {
-//        ImageViewTouch imageView = (ImageViewTouch) player.imageView;
-//
-//        float viewAspect = (float) imageView.getWidth() / imageView.getHeight();
-//        float imageAspect = (float) drawable.getIntrinsicWidth() / drawable.getIntrinsicHeight();
-//
-//        float maxScale = Math.max(1.00001f, viewAspect / imageAspect);
-//        imageView.setImageDrawable(drawable, null, 1, maxScale);
-//        imageView.setDisplayType(ImageViewTouchBase.DisplayType.FIT_TO_SCREEN);
-//
-//        if (atLeast(Build.VERSION_CODES.KITKAT)) {
-//            imageView.setQuickScaleEnabled(true);
-//        }
-//    }
     private void showErrorIndicator() {
         errorIndicator.setVisibility(VISIBLE);
         errorIndicator.setAlpha(0);
         errorIndicator.animate().alpha(1).start();
-    }
-
-    private void showMussteScrollenPopup() {
-        if (singleShotService.isFirstTime("musste_scrollen_hint")) {
-            DialogBuilder.start(getContext())
-                    .content(R.string.hint_musste_scrollen)
-                    .positive()
-                    .show();
-        }
     }
 }
