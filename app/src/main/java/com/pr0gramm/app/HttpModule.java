1777705cc37da6ed4687ddccfcfaaa2ed1faa974
Mopsalarm <mopsalarm@users.noreply.github.com>
2016-02-20 19:22:20 +0100

    Increase read timeout a little bit more.

--- app/src/main/java/com/pr0gramm/app/HttpModule.java
+++ app/src/main/java/com/pr0gramm/app/HttpModule.java
@@ -55,7 +55,7 @@ public class HttpModule {
                 .socketFactory(new SmallBufferSocketFactory())
 
                 .cookieJar(cookieHandler)
-                .readTimeout(25, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
                 .writeTimeout(15, TimeUnit.SECONDS)
                 .connectTimeout(10, TimeUnit.SECONDS)
                 .connectionPool(new ConnectionPool(4, 3, TimeUnit.SECONDS))
