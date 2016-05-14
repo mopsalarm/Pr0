6da535f50042d305b2e309e12737ca4c5f991a44
Mopsalarm <mopsalarm@users.noreply.github.com>
2016-05-14 20:11:21 +0200

    Move 'seen-posts' file from cache to app storage.

--- app/src/main/java/com/pr0gramm/app/services/SeenService.java
+++ app/src/main/java/com/pr0gramm/app/services/SeenService.java
@@ -7,7 +7,9 @@ import android.os.AsyncTask;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.SettableFuture;
 import com.pr0gramm.app.feed.FeedItem;
+import com.pr0gramm.app.util.AndroidUtility;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,15 +34,31 @@ public class SeenService {
 
     @Inject
     public SeenService(Context context) {
-        File file = new File(context.getCacheDir(), "seen-posts.bits");
+        File file = new File(context.getFilesDir(), "seen-posts.bits");
 
         AsyncTask.execute(() -> {
+            // migrate old file if necessary
+            File legacyFile = new File(context.getCacheDir(), "seen-posts.bits");
+            migrate(legacyFile, file);
+
             try {
                 buffer.set(mapByteBuffer(file));
             } catch (IOException error) {
                 logger.warn("Could not load the seen-Cache");
             }
         });
+    }
+
+    private void migrate(File legacyFile, File newFile) {
+        if (legacyFile.exists() && !newFile.exists()) {
+            logger.info("Moving seen-posts file from {} to {}", legacyFile, newFile);
+
+            try {
+                FileUtils.moveFile(legacyFile, newFile);
+            } catch (IOException error) {
+                AndroidUtility.logToCrashlytics(new RuntimeException("Could not migrate 'seen' file.", error));
+            }
+        }
     }
 
     public boolean isSeen(FeedItem item) {
@@ -108,8 +126,8 @@ public class SeenService {
      */
     @SuppressLint("NewApi")
     private static ByteBuffer mapByteBuffer(File file) throws IOException {
-        // space for up to two million posts
-        final long size = 2_000_000 / 8;
+        // space for up to four million posts
+        final long size = 4_000_000 / 8;
 
         logger.info("Mapping cache: " + file);
         try (RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
