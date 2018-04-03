3cad038212baa0b44e8e63b73904874f7e15af8c
Mopsalarm <mopsalarm@users.noreply.github.com>
2018-04-03 20:44:39 +0200

    Update ciphers

--- app/src/main/java/com/pr0gramm/app/HttpModule.kt
+++ app/src/main/java/com/pr0gramm/app/HttpModule.kt
@@ -39,6 +39,7 @@ import java.net.UnknownHostException
 import java.util.concurrent.*
 import kotlin.concurrent.timer
 
+
 /**
  */
 fun httpModule(app: ApplicationClass) = Kodein.Module {
@@ -49,6 +50,19 @@ fun httpModule(app: ApplicationClass) = Kodein.Module {
         val cookieHandler: LoginCookieHandler = instance()
 
         val cacheDir = File(app.cacheDir, "imgCache")
+
+        val spec = ConnectionSpec.Builder(ConnectionSpec.COMPATIBLE_TLS)
+                .tlsVersions(TlsVersion.TLS_1_2, TlsVersion.TLS_1_1, TlsVersion.TLS_1_0)
+                .cipherSuites(
+                        // and more from https://github.com/square/okhttp/issues/3894
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+                        CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+                        CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+                        CipherSuite.TLS_RSA_WITH_AES_256_CBC_SHA,
+                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA)
+                .build()
 
         OkHttpClient.Builder()
                 .cache(okhttp3.Cache(cacheDir, (64 * 1024 * 1024).toLong()))
@@ -62,6 +76,7 @@ fun httpModule(app: ApplicationClass) = Kodein.Module {
                 .retryOnConnectionFailure(true)
                 .dispatcher(Dispatcher(executor))
                 .dns(FallbackDns())
+                .connectionSpecs(listOf(spec, ConnectionSpec.COMPATIBLE_TLS, ConnectionSpec.MODERN_TLS, ConnectionSpec.CLEARTEXT))
 
                 .addNetworkInterceptor(DebugInterceptor())
 
