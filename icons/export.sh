54abf8b648fb2ebecf1ac64bf6c8a7957df3b069
Mopsalarm <mopsalarm@users.noreply.github.com>
2018-06-20 21:14:44 +0200

    Update icons, fix icon export and dpi calculation.

--- icons/export.sh
+++ icons/export.sh
@@ -10,6 +10,9 @@ fi
 SVG=$1
 PNG=$(basename $SVG .svg).png
 
+WIDTH=$(identify -format '%w' $SVG)
+HEIGHT=$(identify -format '%h' $SVG)
+
 FACTOR=1
 if [[ $SVG == *app*.svg ]] ; then
   FACTOR=4
@@ -20,26 +23,30 @@ if [[ $SVG == ic_app.svg || $SVG == ic_roundapp.svg ]] ; then
 	PREFIX=mipmap
 fi
 
-function dpi() {
-	dc -e "6k 90 $1 * $FACTOR 160 * / p"
+function width() {
+	dc -e "6k $1 160 / $FACTOR / $WIDTH * p"
+}
+
+function height() {
+	dc -e "6k $1 160 / $FACTOR / $HEIGHT * p"
 }
 
 mkdir -p ../app/src/main/res/$PREFIX-hdpi
-inkscape -d$(dpi 240) --export-png=../app/src/main/res/$PREFIX-hdpi/$PNG $SVG
+inkscape -w$(width 240) -h$(height 240) --export-png=../app/src/main/res/$PREFIX-hdpi/$PNG $SVG
 optipng -o7 ../app/src/main/res/$PREFIX-hdpi/$PNG
 git add ../app/src/main/res/$PREFIX-hdpi/$PNG
 
 mkdir -p ../app/src/main/res/$PREFIX-xhdpi
-inkscape -d$(dpi 360) --export-png=../app/src/main/res/$PREFIX-xhdpi/$PNG $SVG
+inkscape -w$(width 320) -h$(height 320) --export-png=../app/src/main/res/$PREFIX-xhdpi/$PNG $SVG
 optipng -o7 ../app/src/main/res/$PREFIX-xhdpi/$PNG
 git add ../app/src/main/res/$PREFIX-xhdpi/$PNG
 
 mkdir -p ../app/src/main/res/$PREFIX-xxhdpi
-inkscape -d$(dpi 480) --export-png=../app/src/main/res/$PREFIX-xxhdpi/$PNG $SVG
+inkscape -w$(width 480) -h$(height 480) --export-png=../app/src/main/res/$PREFIX-xxhdpi/$PNG $SVG
 optipng -o7 ../app/src/main/res/$PREFIX-xxhdpi/$PNG
 git add ../app/src/main/res/$PREFIX-xxhdpi/$PNG
 
 mkdir -p ../app/src/main/res/$PREFIX-xxxhdpi
-inkscape -d$(dpi 640) --export-png=../app/src/main/res/$PREFIX-xxxhdpi/$PNG $SVG
+inkscape -w$(width 640) -h$(height 640) --export-png=../app/src/main/res/$PREFIX-xxxhdpi/$PNG $SVG
 optipng -o7 ../app/src/main/res/$PREFIX-xxxhdpi/$PNG
 git add ../app/src/main/res/$PREFIX-xxxhdpi/$PNG
