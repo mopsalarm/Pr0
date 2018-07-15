199039b062efb312bbb945489e8b91cb6747916d
Mopsalarm <mopsalarm@users.noreply.github.com>
2018-07-15 21:34:35 +0200

    Enable StrictMode early on debug.

--- app/src/debug/java/com/pr0gramm/app/DebugApplicationClass.kt
+++ app/src/debug/java/com/pr0gramm/app/DebugApplicationClass.kt
@@ -1,10 +1,15 @@
 package com.pr0gramm.app
 
 import android.content.Context
+import android.os.StrictMode
 import android.support.multidex.MultiDex
 
 
 class DebugApplicationClass : ApplicationClass() {
+    init {
+        StrictMode.enableDefaults()
+    }
+
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
         MultiDex.install(this)
