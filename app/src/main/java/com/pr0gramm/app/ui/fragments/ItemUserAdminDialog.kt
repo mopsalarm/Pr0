74cad40126f23552c19f955a088c72f360cd158d
Quadratball <tim-f@gmx.de>
2019-04-23 20:51:18 +0200

    Update ItemUserAdminDialog.kt

--- app/src/main/java/com/pr0gramm/app/ui/fragments/ItemUserAdminDialog.kt
+++ app/src/main/java/com/pr0gramm/app/ui/fragments/ItemUserAdminDialog.kt
@@ -120,7 +120,7 @@ class ItemUserAdminDialog : BaseDialogFragment("ItemUserAdminDialog") {
                 "Regel #1 - Bild unzureichend getagged (nsfw/nsfl)",
                 "Regel #1 - Falsche/Sinnlose Nutzung des NSFP Filters",
                 "Regel #2 - Gore/Porn/Suggestive Bilder mit Minderjährigen",
-                "Regel #3 - Tierporn",
+                "Regel #3 - Tierporn/Tierquälerei",
                 "Regel #4 - Stumpfer Rassismus/Nazi-Nostalgie",
                 "Regel #5 - Werbung/Spam",
                 "Regel #6 - Infos zu Privatpersonen",
