b245642e8fb1d4cc79b5b708c63e4c8b462f86e8
Mopsalarm <mopsalarm@users.noreply.github.com>
2020-01-04 17:18:21 +0100

    Move the action bar a little up.

--- app/src/main/java/com/pr0gramm/app/ui/Snackbar.kt
+++ app/src/main/java/com/pr0gramm/app/ui/Snackbar.kt
@@ -9,13 +9,14 @@ import com.pr0gramm.app.util.dp
 fun Snackbar.configureNewStyle(): Snackbar {
     val params = view.layoutParams as? ViewGroup.MarginLayoutParams ?: return this
 
-    val margin = context.dp(12f).toInt()
+    val margin = context.dp(12)
     params.setMargins(margin, margin, margin, margin)
 
     val activity = AndroidUtility.activityFromContext(context)
     if (activity is ScrollHideToolbarListener.ToolbarActivity) {
         activity.rxWindowInsets.take(1).subscribe { insets ->
-            params.bottomMargin += insets.bottom
+            val offset = if (insets.bottom == 0) context.dp(64) else insets.bottom
+            params.bottomMargin += offset
         }
     }
 
