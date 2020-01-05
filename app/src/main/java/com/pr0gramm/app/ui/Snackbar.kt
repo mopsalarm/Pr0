b48050fcb24e87287dac7c17ff9e77f2054f3e8e
Mopsalarm <mopsalarm@users.noreply.github.com>
2020-01-05 20:59:46 +0100

    Fix snackbar layout.

--- app/src/main/java/com/pr0gramm/app/ui/Snackbar.kt
+++ app/src/main/java/com/pr0gramm/app/ui/Snackbar.kt
@@ -1,16 +1,27 @@
 package com.pr0gramm.app.ui
 
+import android.graphics.Color
 import android.view.ViewGroup
+import androidx.core.view.isEmpty
 import com.google.android.material.snackbar.Snackbar
 import com.pr0gramm.app.R
 import com.pr0gramm.app.util.AndroidUtility
 import com.pr0gramm.app.util.dp
 
 fun Snackbar.configureNewStyle(): Snackbar {
-    val params = view.layoutParams as? ViewGroup.MarginLayoutParams ?: return this
 
-    val margin = context.dp(12)
-    params.setMargins(margin, margin, margin, margin)
+    // this.view is the layout holding the actual snackbar content.
+    // so we get that one and convert it to a ViewGroup
+    val snackbarLayout = this.view as? ViewGroup ?: return this
+    if (snackbarLayout.isEmpty()) {
+        return this
+    }
+
+    // now we can get the actual snackbar content.
+    val snackbarView = snackbarLayout.getChildAt(0)
+
+    val params = snackbarView.layoutParams as? ViewGroup.MarginLayoutParams ?: return this
+    params.bottomMargin += context.dp(12)
 
     val activity = AndroidUtility.activityFromContext(context)
     if (activity is ScrollHideToolbarListener.ToolbarActivity) {
@@ -20,11 +31,15 @@ fun Snackbar.configureNewStyle(): Snackbar {
         }
     }
 
-    this.view.layoutParams = params
+    snackbarView.layoutParams = params
 
-    this.view.setBackgroundResource(R.drawable.snackbar)
+    snackbarView.setBackgroundResource(R.drawable.snackbar)
 
-    this.view.elevation = context.dp(6f)
+    snackbarView.elevation = context.dp(6f)
+
+    // make layout holding the actual snackbar transparent.
+    snackbarLayout.setBackgroundColor(Color.TRANSPARENT)
+
 
     return this
 }
