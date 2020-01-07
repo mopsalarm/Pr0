5db82bfd52cdc4778384b1291b35d1d8791b3046
Mopsalarm <mopsalarm@users.noreply.github.com>
2020-01-07 09:42:55 +0100

    Fix hubtraffic by redirecting over a app.pr0gramm.com domain.

--- app/src/main/java/com/pr0gramm/app/util/Affiliate.kt
+++ app/src/main/java/com/pr0gramm/app/util/Affiliate.kt
@@ -1,6 +1,6 @@
 package com.pr0gramm.app.util
 
-import android.net.Uri
+import android.util.Base64
 
 object Affiliate {
     private val paramsHubTraffic = mapOf(
@@ -11,20 +11,8 @@ object Affiliate {
     private val reHubTraffic = "(?:pornhub|redtube|tube8|youporn|xtube|spankwire|keezmovies|extremetube)\\.com".toRegex()
 
     private fun hubTraffic(url: String): String {
-        val uri = Uri.parse(url)
-
-        val updated = uri.buildUpon().clearQuery()
-
-        for (name in uri.queryParameterNames - paramsHubTraffic.keys) {
-            val value = uri.getQueryParameter(name) ?: continue
-            updated.appendQueryParameter(name, value)
-        }
-
-        for ((name, value) in paramsHubTraffic) {
-            updated.appendQueryParameter(name, value)
-        }
-
-        return updated.build().toString()
+        val encoded = Base64.encodeToString(url.toByteArray(), Base64.URL_SAFE)
+        return "https://app.pr0gramm.com/redirect.html?u=$encoded"
     }
 
     fun get(url: String): String? {
