4e5161037b4d477aecbdb610d905f9c035e76a72
Mopsalarm <mopsalarm@users.noreply.github.com>
2020-11-22 15:26:41 +0000

    Reduce exoplayer footprint

--- app/src/main/java/com/pr0gramm/app/ui/views/viewer/Recycler.kt
+++ app/src/main/java/com/pr0gramm/app/ui/views/viewer/Recycler.kt
@@ -3,9 +3,15 @@ package com.pr0gramm.app.ui.views.viewer
 import android.content.Context
 import android.os.Handler
 import android.os.Looper
+import com.google.android.exoplayer2.DefaultRenderersFactory
+import com.google.android.exoplayer2.Renderer
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.extractor.mp4.Mp4Extractor
+import com.google.android.exoplayer2.metadata.MetadataOutput
+import com.google.android.exoplayer2.text.TextOutput
 import com.pr0gramm.app.Logger
 import com.pr0gramm.app.time
+import java.util.*
 
 object ExoPlayerRecycler {
     private val logger = Logger("ExoPlayerRecycler")
@@ -55,10 +61,26 @@ object ExoPlayerRecycler {
     }
 
     private fun newExoPlayer(context: Context): SimpleExoPlayer {
+        val ctx = context.applicationContext
+
         logger.debug { "Create new exo player" }
         return SimpleExoPlayer
-                .Builder(context.applicationContext)
+                .Builder(ctx, RenderersFactory(ctx), Mp4Extractor.FACTORY)
                 .build()
+    }
+
+    private class RenderersFactory(ctx: Context) : DefaultRenderersFactory(ctx) {
+        override fun buildCameraMotionRenderers(context: Context, extensionRendererMode: Int, out: ArrayList<Renderer>) {
+        }
+
+        override fun buildMetadataRenderers(context: Context, output: MetadataOutput, outputLooper: Looper, extensionRendererMode: Int, out: ArrayList<Renderer>) {
+        }
+
+        override fun buildMiscellaneousRenderers(context: Context, eventHandler: Handler, extensionRendererMode: Int, out: ArrayList<Renderer>) {
+        }
+
+        override fun buildTextRenderers(context: Context, output: TextOutput, outputLooper: Looper, extensionRendererMode: Int, out: ArrayList<Renderer>) {
+        }
     }
 }
 
